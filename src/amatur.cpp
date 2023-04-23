2994ee7146087954ba144c3a99c7556cff853526
amatur <amatur003@gmail.com>
2023-04-23 15:54:33 -0400

    submod

--- src/amatur.cpp
+++ src/amatur.cpp
@@ -8,32 +8,32 @@
 // #include "permute.cpp"
 
 
-using namespace sshash;
+// using namespace sshash;
 
-int main(int argc, char** argv) {
-    std::string input_filename = "mega.essd";
-    auto k = 31;
-    auto m = k/2;
+// int main(int argc, char** argv) {
+//     std::string input_filename = "mega.essd";
+//     auto k = 31;
+//     auto m = k/2;
 
-    dictionary dict;
+//     dictionary dict;
 
-    build_configuration build_config;
-    build_config.k = k;
-    build_config.m = m;
+//     build_configuration build_config;
+//     build_config.k = k;
+//     build_config.m = m;
 
-    build_config.canonical_parsing = true;
-    build_config.verbose = true;
-    build_config.print();
+//     build_config.canonical_parsing = true;
+//     build_config.verbose = true;
+//     build_config.print();
 
-    dict.build(input_filename, build_config);
-    assert(dict.k() == k);
-    std::cout<<"dict built complete";
+//     dict.build(input_filename, build_config);
+//     assert(dict.k() == k);
+//     std::cout<<"dict built complete";
 
-        // auto output_filename = "amatur_out";
-        // // essentials::logger("saving data structure to disk...");
-        // // essentials::save(dict, output_filename.c_str());
-        // // essentials::logger("DONE");
-    }
+//         // auto output_filename = "amatur_out";
+//         // // essentials::logger("saving data structure to disk...");
+//         // // essentials::save(dict, output_filename.c_str());
+//         // // essentials::logger("DONE");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
