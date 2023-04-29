64540a02fa56a032ecd9612a4767600839502a9a
amatur <amatur003@gmail.com>
2023-04-29 15:11:46 -0400

    str to kmer, before work

--- src/colormatrix.cpp
+++ src/colormatrix.cpp
@@ -138,6 +138,13 @@ void KmerMatrix::get_row(uint64_t row_idx, vector<uint64_t>& to_fill)
 	}
 }
 
+__uint128_t thegetval(uint64_t r) {
+    return ((__uint128_t)r <<  0) |
+           ((__uint128_t)0 << 32) |
+           ((__uint128_t)0 << 64) |
+           ((__uint128_t)0 << 96);
+}
+
 void KmerMatrix::to_color_string_file(const std::string& outfile)
 {
 	//, std::string ess_order_file
@@ -167,7 +174,7 @@ void KmerMatrix::to_color_string_file(const std::string& outfile)
 	{
 		string thekmer=kmer2str(kmers[kmer_idx], this->k);
 		//auto answer = dict.lookup_advanced(thekmer.c_str());
-		auto answer = dict.lookup_advanced_uint(kmers[kmer_idx]);
+		auto answer = dict.lookup_advanced_uint(thegetval(kmers[kmer_idx]));
 		assert(answer.kmer_id != constants::invalid_uint64);
 		if(answer.kmer_id <0 || answer.kmer_id>=this->kmers.size()){
 			cout<<"Erroneus mphf."<<endl;
