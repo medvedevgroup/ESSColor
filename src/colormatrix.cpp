30686a75716f074c4a5425e99dc89a52c0b86acc
amatur <amatur003@gmail.com>
2023-04-29 15:05:51 -0400

    str to kmer

--- src/colormatrix.cpp
+++ src/colormatrix.cpp
@@ -165,10 +165,8 @@ void KmerMatrix::to_color_string_file(const std::string& outfile)
 	cout<<"Total number of k-mers: "<<this->kmers.size()<<endl;
 	for (uint64_t kmer_idx(0) ; kmer_idx<this->kmers.size() ; kmer_idx++)
 	{
-		//
-		//string thekmer=kmer2str(kmers[kmer_idx], this->k);
-		//auto answer = dict.lookup_advanced(thekmer.c_str());
-		auto answer = dict.lookup_advanced_uint(kmers[kmer_idx]);
+		string thekmer=kmer2str(kmers[kmer_idx], this->k);
+		auto answer = dict.lookup_advanced(thekmer.c_str());
 		assert(answer.kmer_id != constants::invalid_uint64);
 		if(answer.kmer_id <0 || answer.kmer_id>=this->kmers.size()){
 			cout<<"Erroneus mphf."<<endl;
