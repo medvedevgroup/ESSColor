3c05767a02b27b3f736e35ca20525020fb8f73c2
amatur <amatur003@gmail.com>
2023-05-01 16:57:55 -0400

    cleaning up before branch push

--- src/colormatrix.cpp
+++ src/colormatrix.cpp
@@ -139,12 +139,14 @@ void KmerMatrix::get_row(uint64_t row_idx, vector<uint64_t>& to_fill)
 }
 
 __uint128_t convert_uint64_to_uint128(uint64_t r) {
+	//if(k<8)
 	// if(k<)
 	// __uint128_t out = 0;
 	// out |= (__uint128_t)r;
 	// out<<64;
     // //return ((__uint128_t)0 << 0) | ((__uint128_t)r << 64);//works in mini, but not ec
-	return (__uint128_t)r;
+	//return (__uint128_t)r;
+	return ((__uint128_t)0 << 0) | ((__uint128_t)r << 64); //doesnt work in mini
 }
 
 void KmerMatrix::to_color_string_file(const std::string& outfile)
